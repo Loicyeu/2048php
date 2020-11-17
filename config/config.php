7c9cbf3fc99a627d23443e91715322d0ff443313
Thowisk <timothe.cabon@etu.univ-nantes.fr>
2020-11-17 19:31:32 +0100

    define config

---
+++ config/config.php
@@ -0,0 +1,12 @@
+<?php
+// Chemin complet de la racine du site sur le serveur.
+define("HOME_SITE", __DIR__."/..");
+
+// Chemin vers les répertoires liés au modèle MVC
+define("PATH_CONTROLEUR", HOME_SITE."/controleur");
+define("PATH_MODELE", HOME_SITE."/modele");
+define("PATH_VUE", HOME_SITE."/vue");
+
+// Données pour la connexion au SGBD
+define("HOST", "");
+define("DB", "");
