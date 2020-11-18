5191d953f00782713075c450036e5eeb2374d325
Loïc Henry <loic.henry2001@gmail.com>
2020-11-18 22:58:28 +0100

    create files

---
+++ controleur/controllerLogin.php
@@ -0,0 +1,9 @@
+<?php
+
+
+class controllerLogin {
+
+    public function __construct() {
+    }
+
+}
--- index.php
+++ index.php
@@ -1,6 +1,4 @@
 <?php
 
 include_once "config/config.php";
-include_once "modele/SqliteConnexion.php";
 
-$con = SqliteConnexion::getInstance();
