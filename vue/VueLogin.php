b6a32981db3cce3c480bbafa8090b3bd0984ae07
Loicyeu <loic.henry2001@gmail.com>
2020-12-04 19:44:38 +0100

    urgence

--- vue/VueLogin.php
+++ vue/VueLogin.php
@@ -13,10 +13,10 @@ class VueLogin {
 //            <input type='submit' value='Se connecter'>
 //        </form>
 //        ";
-
+//padding 50
         echo "
         <div class='text-center h-100'>
-            <form class='form-signin w-25' action='index.php' method='post'>
+            <form class='form-signin w-25 m-auto' action='/index.php' method='post'>
                 <!--<img class='mb-4' src='/docs/4.5/assets/brand/bootstrap-solid.svg' alt='' width='72' height='72'>-->
                 <h1 class='h3 mb-3 font-weight-normal'>Connecter vous pour jouer à 2048</h1>
                 <label for='pseudo' class='sr-only'>Email address</label>
@@ -25,7 +25,9 @@ class VueLogin {
                 <input type='password' id='password' name='password' class='form-control' placeholder='Mot de passe' required>
                 <br>
                 <input type='submit' value='Se connecter'  class='btn btn-lg btn-primary btn-block'>
-                <p class='mt-5 mb-3 text-muted'>© 2020-2021</p>
+                <br>
+                <a href='?register' class='text-success'>Créer un compte ?</a>
+                <p class='mt-5 mb-3 text-muted'>Timothé CABON et Loïc HENRY<br>© 2020-2021</p>
             </form>
         </div>
         ";
