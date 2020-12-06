0ded4cd5fe4af2d122c924409620b54cc49fd9fc
Loicyeu <loic.henry2001@gmail.com>
2020-12-06 11:43:01 +0100

    Add favicon

--- htmlCss/htmlHead.php
+++ htmlCss/htmlHead.php
@@ -2,6 +2,7 @@
 <html lang="fr">
 <head>
     <title>2048</title>
+    <link rel="icon" href="/assets/favicon.ico" />
     <link rel="stylesheet" href="htmlCss/style.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
 </head>
